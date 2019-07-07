1432df8862dd7b245a8d4e6cf6e270aec64b7f2f
Chris Waldron <chriswaldron15@gmail.com>
2019-07-07 11:08:38 +0100

    Initial commit the second

---
+++ Assets/Scenes/Scripts/Server/Server.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Curveball;
+
+namespace VillageBuilder
+{
+    public class Server : CBGGameObject
+    {
+        
+    }
+}
